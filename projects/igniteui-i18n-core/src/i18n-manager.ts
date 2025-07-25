9559e125b847df57f8eb7479875a49eea6d0d1e7
skrustev <striker18@mail.bg>
2025-07-25 20:22:54 +0300

    feat(core): Add currency symbol position implementation.

--- projects/igniteui-i18n-core/src/i18n-manager.ts
+++ projects/igniteui-i18n-core/src/i18n-manager.ts
@@ -124,17 +124,33 @@ export class igI18nManager {
      * Get the currency symbol for provided currency code.
      * @param currencyCode The currency code to get the symbol of.
      * @param currencyDisplay How should the currency code be rendered.
-     * @param locale Override locale.
+     * @param locale Override locale instead of the current one.
      * @returns String representation of the currency symbol.
      */
     public getCurrencySymbol(currencyCode: string, currencyDisplay?: keyof Intl.NumberFormatOptionsCurrencyDisplayRegistry, locale?: string) {
         const options: Intl.NumberFormatOptions = {
             style: 'currency',
             currency: currencyCode,
-            currencyDisplay: currencyDisplay
+            currencyDisplay: currencyDisplay,
+            maximumFractionDigits: 0
         };
         const formatter = this.getNumberFormatter(locale, options);
-        return formatter.formatToParts(0).find((part) => part.type === "currency")?.value;
+        return formatter.formatToParts(0).find(part => part.type === "currency")?.value;
+    }
+
+    /**
+     * Get the currency symbol/name position in formatted value.
+     * @param locale Override locale instead of the current one.
+     * @returns Position of the symbol for number 0.
+     */
+    public getCurrencyPosition(locale?: string) {
+        const options: Intl.NumberFormatOptions = {
+            style: 'currency',
+            currency: "USD",
+            maximumFractionDigits: 0
+        };
+        const formatter = this.getNumberFormatter(locale, options);
+        return formatter.formatToParts(0).findIndex(part => part.type === 'currency');
     }
 
     /**
