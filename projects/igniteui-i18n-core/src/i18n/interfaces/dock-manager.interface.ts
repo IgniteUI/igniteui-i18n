d1f2210c1d0cc84d82616c277b85800f06d6f054
Maya Kirova <MKirova@infragistics.com>
2025-09-12 14:03:21 +0300

    feat(dockManager): Add existing resource strings.

---
+++ projects/igniteui-i18n-core/src/i18n/interfaces/dock-manager.interface.ts
@@ -0,0 +1,11 @@
+export interface IDockManagerResourceStrings {
+  dock_manager_close?: string;
+  dock_manager_pin?: string;
+  dock_manager_unpin?: string;
+  dock_manager_maximize?: string;
+  dock_manager_minimize?: string;
+  dock_manager_more_options?: string;
+  dock_manager_more_tabs?: string;
+  dock_manager_panes?: string;
+  dock_manager_documents?: string;
+}
