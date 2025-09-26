1abd03840cb229b7d919d3ff3014f1329e372709
skrustev <striker18@mail.bg>
2025-09-26 16:51:50 +0300

    chore(*): Add dist cleaning  before build.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@
 
 const gulp = require('gulp');
 const path = require('path');
+const del = require('del');
 
-exports.copyPackageForCore = (cb) => {
+exports.cleanCoreBuild = () => {
+    return del.deleteAsync('dist/igniteui-i18n-core/**', {force:true});
+};
+
+exports.cleanResourcesBuild = () => {
+    return del.deleteAsync('dist/igniteui-i18n-resources/**', {force:true});
+};
+
+exports.copyPackageForCore = () => {
     return gulp
         .src([path.join(__dirname, 'projects/igniteui-i18n-core/package.json')])
         .pipe(gulp.dest(path.join(__dirname, 'dist/igniteui-i18n-core')));
 };
 
-exports.copyPackageForResources = (cb) => {
+exports.copyPackageForResources = () => {
     return gulp
         .src([path.join(__dirname, 'projects/igniteui-i18n-resources/package.json')])
         .pipe(gulp.dest(path.join(__dirname, 'dist/igniteui-i18n-resources')));
