9449a08b54d6e4d4a58abf3087285db887442a56
skrustev <striker18@mail.bg>
2025-10-31 11:30:53 +0200

    fix(resources): Fix extending resources not correctly merging objects.

--- projects/igniteui-i18n-core/src/utils.spec.ts
+++ projects/igniteui-i18n-core/src/utils.spec.ts
@@ -1,4 +1,5 @@
 import { describe, expect, it } from 'vitest';
+import { GridResourceStringsEN } from './i18n/EN/grid-resources.js';
 import { generateLocaleKey } from './utils.js';
 
 describe('util', () => {
@@ -14,5 +15,12 @@ describe('util', () => {
     const keyA = generateLocaleKey('bg', optionsA);
     const keyB = generateLocaleKey('bg', optionsB);
     expect(keyA).equal(keyB);
+  });
+
+  it('grid extended resources should correctly be resolved', () => {
+    expect(GridResourceStringsEN.grid_emptyGrid_message).toEqual('Grid has no data.');
+    expect(GridResourceStringsEN.grid_min_length_validation_error).toEqual(
+      'Entry should be at least {0} character(s) long'
+    );
   });
 });
